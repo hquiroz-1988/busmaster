e99d8bc78918b3f95afbd2f8b835cd80fbdb0028
HQLappy <hugo.quiroz92@gmail.com>
2024-06-23 23:18:47 -0700

    - implementing gate and run function

--- iso_tp/nodeSimulation/src/isotp.cpp
+++ iso_tp/nodeSimulation/src/isotp.cpp
@@ -39,6 +39,91 @@
 /************************************
  * STATIC FUNCTIONS
  ************************************/
+eRetVal_t Isotp::sm_gateFunction(void)
+{
+    switch(currState)
+    {
+        case ISOTPSM_IDLE_STATE:
+        {
+            break;
+        }
+        case ISOTPSM_PROCESS_DATA_STATE:
+        {
+            break;
+        }
+        case ISOTPSM_SEND_SF_STATE:
+        {
+            break;
+        }
+        case ISOTPSM_SEND_FF_STATE:
+        {
+            break;
+        }
+        case ISOTPSM_WAIT_FOR_FC_STATE:
+        {
+            break;
+        }
+        case ISOTPSM_SEND_CF_STATE:
+        {
+            break;
+        }
+        case ISOTPSM_DELAY_CF_STATE:
+        {
+            break;
+        }
+        case ISOTPSM_PROCESS_MSG_STATE:
+        {
+            break;
+        }
+        default:
+        {
+            break;
+        }
+    }
+}
+
+eRetVal_t Isotp::sm_runFunction(void)
+{
+    switch(currState)
+    {
+        case ISOTPSM_IDLE_STATE:
+        {
+            break;
+        }
+        case ISOTPSM_PROCESS_DATA_STATE:
+        {
+            break;
+        }
+        case ISOTPSM_SEND_SF_STATE:
+        {
+            break;
+        }
+        case ISOTPSM_SEND_FF_STATE:
+        {
+            break;
+        }
+        case ISOTPSM_WAIT_FOR_FC_STATE:
+        {
+            break;
+        }
+        case ISOTPSM_SEND_CF_STATE:
+        {
+            break;
+        }
+        case ISOTPSM_DELAY_CF_STATE:
+        {
+            break;
+        }
+        case ISOTPSM_PROCESS_MSG_STATE:
+        {
+            break;
+        }
+        default:
+        {
+            break;
+        }
+    }
+}
 
 /************************************
  * GLOBAL FUNCTIONS
@@ -74,5 +159,13 @@ eRetVal_t Isotp::stateMachineRun(void)
 {
     eRetVal_t eRetVal = ERET_SUCCESS;
 
+    sm_gateFunction();
+
+    
+
+    sm_runFunction();
+
     return eRetVal;
 }
+
+

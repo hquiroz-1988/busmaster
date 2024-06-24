04d093dc80826f83c0dc328ddfcd533d3f8ee50d
HQLappy <hugo.quiroz92@gmail.com>
2024-06-24 15:59:59 -0700

    - creating isotp state machine

--- iso_tp/nodeSimulation/includes/isotp.h
+++ iso_tp/nodeSimulation/includes/isotp.h
@@ -14,6 +14,7 @@
  * INCLUDES
  ************************************/
 #include "typedefs.h"
+#include "statemachine.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -30,12 +31,14 @@ typedef enum
 {
     ISOTPSM_NO_STATE,
     ISOTPSM_IDLE_STATE,
+
     ISOTPSM_PROCESS_DATA_STATE,
     ISOTPSM_SEND_SF_STATE,
     ISOTPSM_SEND_FF_STATE,
-    ISOTPSM_WAIT_FOR_FC_STATE,
+    ISOTPSM_WAIT_FC_STATE,
     ISOTPSM_SEND_CF_STATE,
     ISOTPSM_DELAY_CF_STATE,
+
     ISOTPSM_PROCESS_MSG_STATE,
     ISOTPSM_EXTRACT_SF_STATE,
     ISOTPSM_EXTRACT_FF_STATE,
@@ -43,32 +46,37 @@ typedef enum
     ISOTPSM_WAIT_MSG_STATE,
     ISOTPSM_EXTRACT_CF_STATE,
     ISOTPSM_OUTPUT_DATA_STATE,
-
-
 }isotpSMStates_t;
 
 
-class Isotp
+class Isotp : public StateMachine
 {
     public:
-    isotpSMStates_t prevState;
-    isotpSMStates_t currState;
-    isotpSMStates_t reqState;
-
-
     eRetVal_t debugFunctionCallback(debugFunctionPtr_t dbgFuncPtr);
-
     eRetVal_t testPrint(void);
-
     eRetVal_t stateMachineRun(void);
 
     private:
-
     debugFunctionPtr_t debugPrint;
+    isotpSMStates_t prevState;
+    isotpSMStates_t currState;
+    isotpSMStates_t nextState;
 
-    eRetVal_t sm_gateFunction(void);
+    void update_stateVars(void);
+    void request_nextState(isotpSMStates_t reqState);
 
-    eRetVal_t sm_runFunction(void);
+    bool transition_idleToProcessMsg(void);
+
+    bool transition_idleToProcessData(void);
+    bool transition_processDataToSendSF(void);
+    bool transition_processDataToSendFF(void);
+    bool transition_sendFFToWaitFC(void);
+    bool transition_waitFCToSendCF(void);
+    bool transition_sendCFToDelayCF(void);
+
+
+    void run_idleState(void);
+    
 
 };
 
--- iso_tp/nodeSimulation/src/isotp.cpp
+++ iso_tp/nodeSimulation/src/isotp.cpp
@@ -19,6 +19,8 @@
 /************************************
  * PRIVATE MACROS AND DEFINES
  ************************************/
+#define ENTERING_STATE()    (currState != prevState)
+#define EXITING_STATE()      (currState != nextState)
 
 /************************************
  * PRIVATE TYPEDEFS
@@ -39,90 +41,78 @@
 /************************************
  * STATIC FUNCTIONS
  ************************************/
-eRetVal_t Isotp::sm_gateFunction(void)
+
+void Isotp::update_stateVars(void)
 {
-    switch(currState)
+    prevState = currState;
+    if(ISOTPSM_NO_STATE != nextState)
     {
-        case ISOTPSM_IDLE_STATE:
-        {
-            break;
-        }
-        case ISOTPSM_PROCESS_DATA_STATE:
-        {
-            break;
-        }
-        case ISOTPSM_SEND_SF_STATE:
-        {
-            break;
-        }
-        case ISOTPSM_SEND_FF_STATE:
-        {
-            break;
-        }
-        case ISOTPSM_WAIT_FOR_FC_STATE:
-        {
-            break;
-        }
-        case ISOTPSM_SEND_CF_STATE:
-        {
-            break;
-        }
-        case ISOTPSM_DELAY_CF_STATE:
-        {
-            break;
-        }
-        case ISOTPSM_PROCESS_MSG_STATE:
-        {
-            break;
-        }
-        default:
-        {
-            break;
-        }
+        currState = nextState;
+        nextState = ISOTPSM_NO_STATE;
     }
 }
 
-eRetVal_t Isotp::sm_runFunction(void)
+void Isotp::request_nextState(isotpSMStates_t reqState)
 {
-    switch(currState)
-    {
-        case ISOTPSM_IDLE_STATE:
-        {
-            break;
-        }
-        case ISOTPSM_PROCESS_DATA_STATE:
-        {
-            break;
-        }
-        case ISOTPSM_SEND_SF_STATE:
-        {
-            break;
-        }
-        case ISOTPSM_SEND_FF_STATE:
-        {
-            break;
-        }
-        case ISOTPSM_WAIT_FOR_FC_STATE:
-        {
-            break;
-        }
-        case ISOTPSM_SEND_CF_STATE:
-        {
-            break;
-        }
-        case ISOTPSM_DELAY_CF_STATE:
-        {
-            break;
-        }
-        case ISOTPSM_PROCESS_MSG_STATE:
-        {
-            break;
-        }
-        default:
-        {
-            break;
-        }
-    }
+    nextState = reqState;
+}
+
+void Isotp::run_idleState(void)
+{
+    /* implement run function */
+}
+
+bool Isotp::transition_idleToProcessMsg(void)
+{
+    /* check conditions for transitioning    */
+    bool retVal = false;
+
+    // /* conditions for transitioning are receiving data from CAN module   */
+    // if(get_canMessageFlag())
+    // {
+    //     retVal = true;
+    // }
+
+    return retVal;
+}
+
+bool Isotp::transition_idleToProcessData(void)
+{
+    /* check conditions for transitioning    */
+    bool retVal = false;
+
+    // /* conditions for transitioning are receiving data from UDS module   */
+    // if(get_udsMessageFlag())
+    // {
+    //     retVal = true;
+    // }
+
+    return retVal;
+}
+
+bool Isotp::transition_processDataToSendSF(void)
+{
+    /* check conditions for transitioning    */
+}
+
+bool Isotp::transition_processDataToSendFF(void)
+{
+    /* check conditions for transitioning    */
+}
+
+bool Isotp::transition_sendFFToWaitFC(void)
+{
+    /* check conditions for transitioning    */
+}
+
+bool Isotp::transition_waitFCToSendCF(void)
+{
+    /* check conditions for transitioning    */
+}
+
+bool Isotp::transition_sendCFToDelayCF(void)
+{
+    /* check conditions for transitioning    */
 }
 
 /************************************
@@ -159,11 +149,74 @@ eRetVal_t Isotp::stateMachineRun(void)
 {
     eRetVal_t eRetVal = ERET_SUCCESS;
 
-    sm_gateFunction();
-
     
+    switch(currState)
+    {
+        case ISOTPSM_IDLE_STATE:
+        {
+            if(ENTERING_STATE())
+            {
+                /* first time setup for this state */
+            }
 
-    sm_runFunction();
+            /* run function*/
+            run_idleState();
+            
+
+            /* check for transitions  */
+            if(transition_idleToProcessMsg())
+            {
+                request_nextState(ISOTPSM_PROCESS_DATA_STATE);
+            }
+            else if(transition_idleToProcessData())
+            {
+                request_nextState(ISOTPSM_PROCESS_DATA_STATE);
+            }
+            else
+            {
+                /* do nothing, possibly clear the req state if not one of the above states */
+            }
+
+
+            if(EXITING_STATE())
+            {
+                /* last minute wrap up with this state   */
+            }
+            break;
+        }
+        case ISOTPSM_PROCESS_DATA_STATE:
+        {
+            break;
+        }
+        case ISOTPSM_SEND_SF_STATE:
+        {
+            break;
+        }
+        case ISOTPSM_SEND_FF_STATE:
+        {
+            break;
+        }
+        case ISOTPSM_WAIT_FOR_FC_STATE:
+        {
+            break;
+        }
+        case ISOTPSM_SEND_CF_STATE:
+        {
+            break;
+        }
+        case ISOTPSM_DELAY_CF_STATE:
+        {
+            break;
+        }
+        case ISOTPSM_PROCESS_MSG_STATE:
+        {
+            break;
+        }
+        default:
+        {
+            break;
+        }
+    }
 
     return eRetVal;
 }
